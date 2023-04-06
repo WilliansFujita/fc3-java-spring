f68b8ff527f038e142c6599231354fc8f0069b50
Willians <williansvf@hotmail.com>
2023-04-05 22:05:17 -0300

    initial commit

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/UseCaseTest.java
@@ -0,0 +1,13 @@
+package com.fullcycle.admin.catalogo;
+
+import com.fullcycle.admin.catalogo.application.UseCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UseCaseTest {
+
+    @Test
+    public void testCreateuserCase(){
+        Assertions.assertNotNull(new UseCase().execute());
+    }
+}

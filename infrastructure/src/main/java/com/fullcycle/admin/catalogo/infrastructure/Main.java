1ceeb6ca9a4ff258001f76475caa55904c9bf90b
Willians <williansvf@hotmail.com>
2023-06-10 09:27:59 -0300

    injeção de usecases

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,8 +1,13 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.application.category.create.CreateCategoryUseCase;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
